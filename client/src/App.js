c121c1d3c2824c18eee5b23a9005572cfb504246
giriritesh123 <giriritesh7388@gmail.com>
2023-09-27 23:26:19 +0530

    remove admin

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,6 @@ import CartScreen from "./screens/CartScreen";
 import Registe from "./screens/Registe";
 import Login from "./screens/Login";
 import OrderScreen from "./screens/OrderScreen";
-import AdminScreen from "./screens/AdminScreen";
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
       <TopBar />
       <NavBar />
       <Switch>
-        <Route path="/admin" component={AdminScreen} />
         <Route path="/orders" component={OrderScreen} exact />
         <Route path="/login" component={Login} exact />
         <Route path="/register" component={Registe} exact />
